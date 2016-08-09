3c31cc7a1e94a67c2209ab06b77416dfad6f6334
Willson Mock <wmock@palantir.com>
2016-08-09 16:52:45 -0700

    Clean up css for Education section

--- src/components/Education/Education.js
+++ src/components/Education/Education.js
@@ -13,15 +13,18 @@ class Education extends Component {
     }
 
     renderEducationList() {
-        const educationList = education.map(({ name, degree, major, startYear, endYear, location }) => {
+        const bullet = <span className="bullet">&bull;</span>;
+        const educationList = education.map(({ name, url, degree, major, startYear, endYear }) => {
+            const degreeMajor = degree && major && <span className="Education__degree">{degree}, {major}{bullet}</span>;
             return (
                 <li key={name + startYear}className="Education__list_item">
-                    <h3>Name: {name}</h3>
-                    <p>Degree: {degree}</p>
-                    <p>Major: {major}</p>
-                    <p>Start Year: {startYear}</p>
-                    <p>End Year: {endYear}</p>
-                    <p>Location: {location}</p>
+                    <h3 className="Education__name"><a href={url} className="Education__url">{name}</a></h3>
+                    <p className="Education__degree_dates">
+                        {degreeMajor}
+                        <span className="Education__start_year">{startYear}</span>
+                        <span> &ndash; </span>
+                        <span className="Education__endYear">{endYear}</span>
+                    </p>
                 </li>
             );
         });
