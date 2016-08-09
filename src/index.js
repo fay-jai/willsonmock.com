1170941382f28eda72e3728100731166b7235200
Willson Mock <wmock@palantir.com>
2016-08-09 11:47:49 -0700

    Add normalize and remove bullet and padding from social list

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App/App.js";
+import "normalize.css";
 import "./index.css";
 
 ReactDOM.render(
-  <App />,
-  document.getElementById("root")
+    <App />,
+    document.getElementById("root")
 );
