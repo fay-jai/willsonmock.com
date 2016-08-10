305700752e7daacd73b924bb7dc6feba809324b5
Willson Mock <wmock@palantir.com>
2016-08-09 18:00:54 -0700

    Minor change to blurb contact button

--- src/components/AboutMe/Blurb.js
+++ src/components/AboutMe/Blurb.js
@@ -5,7 +5,7 @@ export default ({ blurb, email }) => {
     return (
         <div className="Blurb">
             <p className="Blurb__text">{blurb}</p>
-            <a href={`mailto:${email}`} className="Blurb__contact_button">Contact me</a>
+            <a href={`mailto:${email}`} className="Blurb__contact_button">Say Hi!</a>
         </div>
     );
 };
